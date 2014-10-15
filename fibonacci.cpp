fc78ac9c24e98ceaaa8bf16c5029fc3973138c7a
PalashBansal96 <PalashBansal96@gmail.com>
2014-10-15 21:48:37 +0530

    Bug + addition

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -7,8 +7,16 @@ int fib(int n){
 int fib2(int n, int a, int b){
 	return n==1?b:fib2(n-1,b,a+b);
 }
+int fib3(int n){
+	int  a = 0, b =1,i;
+	for (i=0;i<n;i++){
+		a = a+b; b = a-b;
+	}
+	return a;
+}
 int main(){
 	cout<<fib(10)<<endl;
-	cout<<fib(10)<<endl;
+	cout<<fib2(10,0,1)<<endl;
+	cout<<fib3(10)<<endl;
 	return 0;
 }
