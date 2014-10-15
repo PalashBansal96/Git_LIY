f5a4b56cb81f0980195c0a97ff21435a343e22f8
PalashBansal96 <PalashBansal96@gmail.com>
2014-10-15 21:33:14 +0530

    Better approach

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int fib(int n){
 	return (n<=2)?1:(fib(n-1)+fib(n-2));
 }
-
+int fib2(int n, int a, int b){
+	return n==1?b:fib2(n-1,b,a+b);
+}
 int main(){
+	cout<<fib(10)<<endl;
 	cout<<fib(10)<<endl;
 	return 0;
 }
